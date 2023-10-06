7de965e3b806cf6b13570ac1c908878f233b7beb
Muhammad Aitisam Yaseen <145204832+MaitisamY@users.noreply.github.com>
2023-10-06 23:41:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let images = ['./images/dice1.png', './images/dice2.png', './images/dice3.png', 
-'./images/dice4.png', './images/dice5.png', './images/dice6.png'];
-let randomNumberOne = Math.floor(Math.random(5) * 5);
-let randomNumberTwo = Math.floor(Math.random(5) * 5);
+let images = ['dice1.png', 'dice2.png', 'dice3.png', 
+'dice4.png', 'dice5.png', 'dice6.png'];
+let randomNumberOne = Math.floor(Math.random() * 6);
+let randomNumberTwo = Math.floor(Math.random() * 6);
 document.querySelector(".img1").setAttribute("src", images[randomNumberOne]);
 document.querySelector(".img2").setAttribute("src", images[randomNumberTwo]);
 
